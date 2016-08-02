61f30041c444cb77947a6c3615674507f6eee951
Seagyn <seagyn@digitalleap.co.za>
2016-08-02 21:48:13 +0200

    Trying to fix the plugin header

--- the-social-links.php
+++ the-social-links.php
@@ -1,34 +1,36 @@
 <?php
 /**
- *	The Social Links
- *
- *	Plugin Name: The Social Links
- *	Plugin URI: http://digitalleap.co.za/wordpress/plugin/the-social-links/
- *	Description: The Social Links plugin adds a widget and shortcode to your WordPress website allowing you to display icons linking to your social profiles.
- *	Version: 1.3
- *	Author: Digital Leap
- *	Author URI: http://digitalleap.co.za/
- *	License: GPL2
- *	Text Domain: the-social-links
- *
- *	Copyright 2016 Digital Leap (email : info@digitalleap.co.za)
- *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License, version 2, as
- *	published by the Free Software Foundation.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * The Social Links
  *
  * @package   TheSocialLinks
  * @category  Class
  * @author    Digital Leap
+ */
+
+/*
+	Plugin Name: The Social Links
+	Plugin URI: http://digitalleap.co.za/wordpress/plugin/the-social-links/
+	Description: The Social Links plugin adds a widget and shortcode to your WordPress website allowing you to display icons linking to your social profiles.
+	Version: 1.3
+	Author: Digital Leap
+	Author URI: http://digitalleap.co.za/
+	License: GPL2
+	Text Domain: the-social-links
+
+	Copyright 2016 Digital Leap (email : info@digitalleap.co.za)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 /**
