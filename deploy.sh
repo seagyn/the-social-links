2f284b769617eb3ece20866a634a7151489b650c
Seagyn <seagyn@digitalleap.co.za>
2016-08-03 18:13:23 +0200

    Tagging version 1.2

--- deploy.sh
+++ deploy.sh
@@ -59,28 +59,28 @@ git push origin master --tags
 
 echo
 echo "Creating local copy of SVN repo ..."
-git svn co $SVNURL $SVNPATH
+svn co $SVNURL $SVNPATH
 
 echo "Clearing svn repo so we can overwrite it"
-git svn rm $SVNPATH/trunk/*
+svn rm $SVNPATH/trunk/*
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific files and deployment script"
-git svn propset -q -R svn:ignore -F .svnignore
+svn propset -q -R svn:ignore -F .svnignore
 
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
 # Add all new files that are not set to be ignored
-git svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-git svn commit --username=$SVNUSER -m "$COMMITMSG"
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn commit --username=$SVNUSER -m "$COMMITMSG"
 
 echo "Creating new SVN tag & committing it"
 cd $SVNPATH
-git svn copy trunk/ tags/$NEWVERSION1/
+svn copy trunk/ tags/$NEWVERSION1/
 cd $SVNPATH/tags/$NEWVERSION1
-git svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 echo "Removing temporary directory $SVNPATH"
 rm -fr $SVNPATH/
