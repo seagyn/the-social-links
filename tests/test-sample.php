fb0c7dccd9992072ce865660a87cdb2da6e79e58
Seagyn <seagyn@digitalleap.co.za>
2016-08-12 01:24:15 +0200

    fixed folder transversing during tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -17,4 +17,13 @@ class SampleTest extends WP_UnitTestCase {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
+
+	/**
+	 * Ensure that the plugin has been installed and activated.
+	 */
+	function test_plugin_activated() {
+
+		$this->assertTrue( is_plugin_active( 'the-social-links/the-social-links.php' ) );
+
+	}
 }
