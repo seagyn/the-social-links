b0845767c42c72dec51a8cb055f48cdac4024079
Seagyn <seagyn@digitalleap.co.za>
2016-08-12 01:54:22 +0200

    Removed unit test for now.

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -18,12 +18,4 @@ class SampleTest extends WP_UnitTestCase {
 		$this->assertTrue( true );
 	}
 
-	/**
-	 * Ensure that the plugin has been installed and activated.
-	 */
-	function test_plugin_activated() {
-
-		$this->assertTrue( is_plugin_active( 'the-social-links/the-social-links.php' ) );
-
-	}
 }
