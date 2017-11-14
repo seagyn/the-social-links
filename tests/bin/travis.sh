4618a090d9f80a372bd8bfcea2dc2880e4474e45
Seagyn <seagyn@digitalleap.co.za>
2017-11-14 08:45:28 +0200

    Fix travis tests

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -7,6 +7,6 @@ plugin_dir=$(pwd)
 mkdir $WP_TESTS_DIR/php-codesniffer && curl -L https://github.com/squizlabs/PHP_CodeSniffer/archive/master.tar.gz | tar xz --strip-components=1 -C $WP_TESTS_DIR/php-codesniffer
 mkdir $WP_TESTS_DIR/wordpress-coding-standards && curl -L https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/archive/master.tar.gz | tar xz --strip-components=1 -C $WP_TESTS_DIR/wordpress-coding-standards
 
-$WP_TESTS_DIR/php-codesniffer/scripts/phpcs --config-set installed_paths $WP_TESTS_DIR/wordpress-coding-standards
+$WP_TESTS_DIR/php-codesniffer/bin/phpcs --config-set installed_paths $WP_TESTS_DIR/wordpress-coding-standards
 
 phpenv rehash
